10cc9824594f2e1b6846d4663216239f06459cd0
briceloubet2001-web <briceloubet2001@gmail.com>
2026-02-15 12:43:17 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,29 @@
-// Version du cache
-const CACHE_NAME = 'pentatrack-v12';
+const CACHE_NAME = 'pentatrack-v13';
 
 self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(['/']);
+    })
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil(clients.claim());
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+  clients.claim();
 });
 
-// Gestionnaire de fetch obligatoire pour la PWA mais configuré en pur "pass-through"
 self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
 });
